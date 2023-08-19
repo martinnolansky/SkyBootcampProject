f957576d67d9bf5984a783a9351a7e8477f49ce6
Martin Nolan <martinnolan_1@live.co.uk>
2023-08-19 21:15:16 +0100

    Code tidied and commented

--- Readme.md
+++ Readme.md
@@ -1,11 +1,9 @@
 Sky Bootcamp Group Project
 
-Very basic structure for website / Template
-
 ## Getting Started
 
 1. clone the repository
-2. open vs code
+2. open vsCode
 3. cd into folder from terminal
 4. run command `python3 -m pip install -r requirements.txt`
 5. to start server run command `python home.py`
--- home.py
+++ home.py
@@ -1,3 +1,4 @@
+#Imports 
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
@@ -6,36 +7,41 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
 
+
+# Instantiate and connect to local database
 app = Flask(__name__)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey' # Should be in an env file
 
+
+# Flask-Login package for handling user session management
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+# Create table and columns in local database
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
 
 
+# Flask form to allow user to register
 class RegisterForm(FlaskForm):
     username = StringField(validators=[
                            InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-
     password = PasswordField(validators=[
                              InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
     submit = SubmitField('Register')
 
+
+# Check to see if the username entered in register form already exists in database
     def validate_username(self, username):
         existing_user_username = User.query.filter_by(
             username=username.data).first()
@@ -44,6 +50,7 @@ class RegisterForm(FlaskForm):
                 'That username already exists. Please choose a different one.')
 
 
+# Flask form to allow user to login
 class LoginForm(FlaskForm):
     username = StringField(validators=[
                            InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
@@ -54,10 +61,25 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 
-
+# Flask routes linking to html templates
 @app.route('/')
 def home():
    return render_template('home.html')
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.errorhandler(404)
+def pageNotFound(e):
+    return render_template('404.html'), 404
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -70,36 +92,16 @@ def login():
                 return redirect(url_for('dashboard'))
     return render_template('login.html', form=form)
 
-
-@app.route('/dashboard', methods=['GET', 'POST'])
-@login_required
-def dashboard():
-    return render_template('dashboard.html')
-
-
-@app.route('/logout', methods=['GET', 'POST'])
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('home'))
-
-
 @ app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
         new_user = User(username=form.username.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
-
     return render_template('register.html', form=form)
-
-@app.errorhandler(404)
-def pageNotFound(e):
-    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run()
